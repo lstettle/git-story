fda3ee0f594506564d2bcadd350c58cdcb892b7f
Linda Stettler <lstettle@yahoo.com>
2017-02-27 19:01:28 -0600

    Added text and vaiable

--- git-story.rb
+++ git-story.rb
@@ -1,1 +1,4 @@
-This is the story of the first day of class. Tomorrow will be the second day of class. After that will be the third day of class.
+"This is the story of the first day of class. Tomorrow will be the second day of class. After that will be the third day of class.
+I am now adding more to this file"
+X = x + 1
+puts x
